5f94378fa8ba3f1becc56fd3988cba39fd59383b
halmanza <anthonya771@gmail.com>
2020-07-24 16:58:41 -0700

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Checkbox() {
+  const [checked,toggle] = useReducer((checked) => !checked,false);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <>
+      <input type="checkbox" value={checked} onChange={toggle} />
+      {checked ? "checked" : "not checked"}
+    </>
+  );
+}
+
+function App() {
+  return <Checkbox />;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
